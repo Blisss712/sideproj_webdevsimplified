c7471d2a48755bd0daeb177fc36b08d7657ba470
Blisss712 <derekjoshreyes132@gmail.com>
2025-01-16 13:36:57 +0800

    added function

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 testing = [1,2,3]
 for test in testing:
-    print("TesGayut")
+    print("hell nah")
+
+def testfunc():
+    counter = 0
+    while counter < 5:
+        print("nah")
+        counter+=1
